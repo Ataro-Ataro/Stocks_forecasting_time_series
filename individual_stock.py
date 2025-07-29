c4147c36cc1d730ef9c666755e8309b7a55c8fcf
Ataro-Ataro <ataroataro1@gmail.com>
2025-07-29 07:38:34 -0700

    Update individual_stock.py

--- individual_stock.py
+++ individual_stock.py
@@ -1,8 +1,12 @@
 import pandas as pd
 
-merged_df = pd.read_pickle('merged_nse_df.pkl')
+@st.cache_data
+    def load_data():
+        return pd.read_pickle("merged_nse_df.pkl")
+    merged_df = load_data()
 
 def get_stock(merged_df, stock_name):
     individual_stock = merged_df[merged_df['Name'] == stock_name].copy()
+    individual_stock["Date"] = pd.todatetime(individual_stock["Date"],errors = "coerce")
     individual_stock = individual_stock.sort_values('Date')
     return individual_stock
